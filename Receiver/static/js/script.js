a5bc2354abf493af1717b4e31d2456c2511bd2b6
Nehal Khan <128951002+Nehal-Khan-29@users.noreply.github.com>
2025-03-03 13:09:54 +0530

    Update script.js

--- Receiver/static/js/script.js
+++ Receiver/static/js/script.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
     let audio = document.getElementById("audioPlayerrecv");
-    audio.src = "static/assets/audio/received_audio.mp3"; // Set the latest file
+    audio.src = "static/assets/audio/decrypted_audio.mp3"; // Set the latest file
     audio.load(); // Ensure it reloads
 });
